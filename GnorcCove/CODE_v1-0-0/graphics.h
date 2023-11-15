d56da4c452726b1d7ae6bc5fbfb6eb45abea206d
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2023-11-15 01:55:13 +0100

    Removed v1.0.0 code files.

--- GnorcCove/CODE_v1-0-0/graphics.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void TITLECARDS_GnorcCove(const char* path, const HelperFunctions& helperFunctions);
-void MISSIONCARDS_GnorcCove();
-void CHECK_LanternEngine();
--- GnorcCove/CODE_v1-0-0/o_skybox.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void LOAD_GCSkybox();
